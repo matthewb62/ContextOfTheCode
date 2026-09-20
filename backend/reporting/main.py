2c53c176667d0a966fca8b9247830b1868ec013d
matthewb62 <24416827@studentmail.ul.ie>
2026-09-20 16:21:02 +0100

    Fix CORS: remove allow_credentials from reporting service

--- backend/reporting/main.py
+++ backend/reporting/main.py
@@ -27,7 +27,6 @@ app = FastAPI(title="MetricsFlow Reporting", lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials = True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
